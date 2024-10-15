d45ec355623130168b11d605b7eb3b2ac5096842
Jens Andreasson <jens.andreasson@ntig.se>
2024-10-15 10:24:43 +0200

    with body parser

--- routes/quiz.js
+++ routes/quiz.js
@@ -14,11 +14,22 @@ router.get("/questions", (req, res) => {
       answers: [3, 12, 5, 4],
       correctAnswer: 4
     },
+    {
+      text: "Hur många ben har en gnu?",
+      answers: ["två", "sju", "tre", "fyra"],
+      correctAnswer: "fyra",
+    }
   ]
   res.render("questions.njk", {
     message: "Frågor",
     questions
   })
+})
+
+router.post("/end", (req, res) => {
+  const answers = req.body
+  console.log(answers)
+  res.json(answers)
 })
 
 export default router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import nunjucks from "nunjucks"
 import morgan from "morgan"
+import bodyParser from "body-parser"
 
 import indexRouter from "./routes/index.js"
 import searchRouter from "./routes/search.js"
@@ -14,9 +15,11 @@ nunjucks.configure("views", {
 })
 app.use(express.static("public"))
 app.use(morgan("dev"))
-app.use("/search", searchRouter)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/", indexRouter)
+app.use("/search", searchRouter)
 app.use("/quiz", quizRouter)
 
 // testa med att surfa localhost:3000/asdfhjk
